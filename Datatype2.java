4147a5b6c744e45f3789a363daaab86ea734d7ea
Praba <svinupraba@gmail.com>
2022-03-09 14:27:19 +0530

    vinoth file

---
+++ Datatype2.java
@@ -0,0 +1,17 @@
+package com.Datatype;
+
+public class Datatype2{
+	//public static void main(String[] args) {
+	public void gran() {
+
+	
+		int number = 10 ;
+		char sex = ('a');
+		String name =("praba");
+		System.out.println(number);
+		System.out.println(sex);
+		System.out.println(name);
+		
+	}
+
+}
