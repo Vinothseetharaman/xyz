2bf1bc2f4749c250dd7c08ef8ac726ab743a8a6d
Praba <svinupraba@gmail.com>
2022-03-10 23:57:48 +0530

    Balaa

---
+++ TestSenorio.java
@@ -0,0 +1,18 @@
+package com.Datatype;
+
+public class TestSenorio {
+	
+	public static void main(String[] args) {
+		
+		//ageOfPerson 18 and indianCitizen is eligible to vote
+		//Using relative operators
+		
+		int ageOfPerson = 18;
+		boolean indianCitizen = true;
+		
+		System.out.println(ageOfPerson == 18 && indianCitizen == true);
+		
+		
+	}
+
+}
